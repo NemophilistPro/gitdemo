b50dd594d3bb60e2b12d79b4a89094c3cf82fb9a
NemophilistPro <765119588@qq.com>
2021-03-12 18:06:35 +0800

    1

---
+++ src/main/java/Demo1.java
@@ -0,0 +1,11 @@
+/**
+ * @Author NemophilistPro
+ * @Description TODO
+ * @Date 2021/3/12 17:59
+ * @Version 1.0
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
